f2f64e739d6a5c2389196ee0df4116f36b1cc127
xyz <981378964@qq.com>
2021-07-24 22:00:48 +0800

    init

---
+++ src/main/java/com/xyz/study/jike/week5/task2/User.java
@@ -0,0 +1,19 @@
+package com.xyz.study.jike.week5.task2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Zhu WeiJie
+ * @date 2021/7/24
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private int id;
+    private String name;
+
+}
---
+++ src/main/java/com/xyz/study/jike/week5/task2/UserService.java
@@ -0,0 +1,15 @@
+package com.xyz.study.jike.week5.task2;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Zhu WeiJie
+ * @date 2021/7/24
+ **/
+@Service
+public class UserService {
+
+    public void printUser(User user){
+        System.out.println(user);
+    }
+}
