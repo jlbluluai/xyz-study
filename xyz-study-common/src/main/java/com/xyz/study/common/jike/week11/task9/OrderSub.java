9fae3f6ee95e5f738576f8579af2ef5b8bcb01a8
xyz <981378964@qq.com>
2021-09-03 15:42:53 +0800

    week11

--- xyz-study-common/src/main/java/com/xyz/study/common/jike/week11/task9/OrderSub.java
+++ xyz-study-common/src/main/java/com/xyz/study/common/jike/week11/task9/OrderSub.java
@@ -1,6 +1,7 @@
 package com.xyz.study.common.jike.week11.task9;
 
 import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,13 @@ import org.springframework.stereotype.Service;
  * @date 2021/9/2
  **/
 @Service
+@Slf4j
 public class OrderSub implements MessageListener {
 
     @Override
     public void onMessage(Message message, byte[] bytes) {
         String s = new String(message.getBody());
         OrderAddVO orderAddVO = JSON.parseObject(s, OrderAddVO.class);
-        System.out.println("添加订单：" + orderAddVO);
+        log.info("添加订单 param={}", orderAddVO);
     }
 }
